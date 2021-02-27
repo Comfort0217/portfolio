f121f353edd885828e45019dce9b5acc21d59eec
Comfort0217 <carp041800@icloud.com>
2021-02-28 01:59:48 +0900

    syusei7

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -54,11 +54,29 @@ if($max==0)
 	exit();
 }
 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$dbh = new PDO(
+  'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+  $username,
+  $password,
+  [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+  ]
+	);
+
+
+// $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+// $user='root';
+// $password='';
+// $dbh=new PDO($dsn,$user,$password);
+// $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 foreach($cart as $key=>$val)
 {
