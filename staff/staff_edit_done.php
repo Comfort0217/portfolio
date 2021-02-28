40725519f2f5f6557c7abac560a5aaa598c38ea5
Comfort0217 <carp041800@icloud.com>
2021-02-28 09:57:59 +0900

    syusei18

--- staff/staff_edit_done.php
+++ staff/staff_edit_done.php
@@ -23,6 +23,23 @@ if(isset($_SESSION['login'])==false)
   try
   {
 
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    
+    $dbh = new PDO(
+      'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+      $username,
+      $password,
+      [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+      ]
+      );
+
     require_once('../common/common.php');
 
     $post=sanitize($_POST);
@@ -31,11 +48,12 @@ if(isset($_SESSION['login'])==false)
     $staff_name=$post['name'];
     $staff_pass=$post['pass'];
     
-    $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-    $user='root';
-    $password='';
-    $dbh=new PDO($dsn,$user,$password);
-    $dbh-> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    // $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+    // $user='root';
+    // $password='';
+    // $dbh=new PDO($dsn,$user,$password);
+    // $dbh-> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
     $sql='UPDATE mst_staff SET name=?,password=? WHERE code=?';
     $stmt = $dbh->prepare($sql);
     $date[] = $staff_name;
