4445b9e238013940233e793a346c73ab040c1010
Comfort0217 <carp041800@icloud.com>
2021-02-28 10:50:16 +0900

    syusei28

--- shop/member_login_check.php
+++ shop/member_login_check.php
@@ -3,6 +3,23 @@
 try
 {
 
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+	
+	$dbh = new PDO(
+		'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+		$username,
+		$password,
+		[
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+		]
+		);	
+
 require_once('../common/common.php');
 
 $post=sanitize($_POST);
@@ -11,11 +28,11 @@ $member_pass=$post['pass'];
 
 $member_pass=md5($member_pass);
 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+// $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+// $user='root';
+// $password='';
+// $dbh=new PDO($dsn,$user,$password);
+// $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 $sql='SELECT code,name FROM dat_member WHERE email=? AND password=?';
 $stmt=$dbh->prepare($sql);
