5e7eb65d806b43363fbad3973cce18d7db906445
Comfort0217 <carp041800@icloud.com>
2021-02-28 09:44:50 +0900

    syusei14

--- staff/staff_disp.php
+++ staff/staff_disp.php
@@ -21,13 +21,30 @@ if(isset($_SESSION['login'])==false)
 
 try{
 
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+  
+  $dbh = new PDO(
+    'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+    $username,
+    $password,
+    [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    ]
+    );
+
   $staff_code=$_GET['staffcode'];
 
-  $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-  $user='root';
-  $password='';
-  $dbh=new PDO($dsn,$user,$password);
-  $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  // $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+  // $user='root';
+  // $password='';
+  // $dbh=new PDO($dsn,$user,$password);
+  // $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
   $sql='SELECT name FROM mst_staff WHERE code=?';
   $stmt = $dbh->prepare($sql);
