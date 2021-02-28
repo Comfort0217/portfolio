395ffe8d0da53263e52cecf4c37187a1450ae8a6
Comfort0217 <carp041800@icloud.com>
2021-02-28 11:12:27 +0900

    syusei35

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -13,26 +13,27 @@
 
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$dbh = new PDO(
-	'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
-	$username,
-	$password,
-	[
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
-	]
-	);
 
 try
 {
 
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+	
+	$dbh = new PDO(
+		'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+		$username,
+		$password,
+		[
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+		]
+		);	
+	
 	
 require_once('../common/common.php');
 
