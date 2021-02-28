3a6debd7b107e47efa297702f5696408b3ee963a
Comfort0217 <carp041800@icloud.com>
2021-02-28 10:55:11 +0900

    syusei30

--- shop/shop_kantan_done.php
+++ shop/shop_kantan_done.php
@@ -22,6 +22,23 @@
 try
 {
 
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+	
+	$dbh = new PDO(
+		'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+		$username,
+		$password,
+		[
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+		]
+		);	
+
 require_once('../common/common.php');
 
 $post=sanitize($_POST);
@@ -56,11 +73,11 @@ $cart=$_SESSION['cart'];
 $kazu=$_SESSION['kazu'];
 $max=count($cart);
 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+// $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+// $user='root';
+// $password='';
+// $dbh=new PDO($dsn,$user,$password);
+// $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 for($i=0;$i<$max;$i++)
 {
